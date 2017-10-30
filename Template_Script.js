13c8999eb077b9ab39982b26fcf898ccf3ad0f37
Dana Kimball <dkimball@ucsd.edu>
2017-10-30 16:47:59 -0700

    Created Template HTML Doc

---
+++ Template_Script.js
@@ -0,0 +1,15 @@
+$('#element_id').click(doSomething1);
+$('element_type').click(doSomething2);
+$('.element_class').click(doSomething3);
+
+function doSomething1(event) {
+	$(this).hide();
+}
+
+function doSomething2(event) {
+	$(this).hide();
+}
+
+function doSomething3(event) {
+	$(this).hide();
+}
