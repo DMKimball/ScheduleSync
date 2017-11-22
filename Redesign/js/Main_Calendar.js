fa82b5cfded0b10b8f06fe035a7b34c7d0e5d4c7
izeferino94 <izeferin@ucsd.edu>
2017-11-21 20:12:12 -0800

    events showing on calendar, fixed multi day events bug not showing across months and year

--- Redesign/js/Main_Calendar.js
+++ Redesign/js/Main_Calendar.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
 function genRow(d1,d2,d3,d4,d5,d6,d7) {
   var td1 = '<td class=element>'+d1+'</td>'; var td2 = '<td class=element>'+d2+'</td>';
-  var td3 = '<td class=element>'+d3+'</td>'; var td4 = '<td class=element>'+d4+'</td>';
+  var td3 = '<td class=element>'+d3+'</ul></td>'; var td4 = '<td class=element>'+d4+'</td>';
   var td5 = '<td class=element>'+d5+'</td>'; var td6 = '<td class=element>'+d6+'</td>';
   var td7 = '<td class=element>'+d7+'</td>';
   $('#main_calendar').append('<tr class=day_rows>'+td1+td2+td3+td4+td5+td6+td7+'</tr>')
@@ -26,15 +26,23 @@ function generateDays(date) {
   date.setDate(1);
   var isFinal = false;
   var daysArray;
-  var start;
+  var eventsArray;
+  var start=0;
   var end;
+  var events = '';
 
   while(isFinal == false) {
     daysArray=['','','','','','','',''];
+    eventsArray=['','','','','','','',''];
     start = date.getDay();
     end = 6-start;
     for(end; end>=0; end--) {
-      daysArray[start]=date.getDate();
+      daysArray[start]=date.getDate()+'<ul>'+generateEvents(date);
+      //Get events here ----------------------
+      //eventsArray[start]=generateEvents(date);
+      console.log(eventsArray[2]);
+      //console.log(eventsArray[daysArray[start]]);
+      //
       date.setDate(date.getDate()+1);
       if(date.getDate()==1){
         isFinal = true;
@@ -42,11 +50,53 @@ function generateDays(date) {
       }
       start++
     }
+    //var tues = eventsArray[2];
+    //console.log(eventsArray);
     genRow(daysArray[0],daysArray[1],daysArray[2],daysArray[3],daysArray[4],daysArray[5],daysArray[6]);
   }
 }
 
+function generateEvents(date) {
+  var stringOfEvents=''
+  if (localStorage.getItem('event') != null) {
+    var current = JSON.parse(localStorage.getItem('event'));
 
+    current.sort(function(a,b){
+      return new Date(a.start) - new Date(b.start);
+    });
+
+    for (var i = 0; i < current.length; i++) {
+      var owner = localStorage.getItem('username');
+      var gen = false;
+
+      //gen <- true if current user is on event contact
+      for(var j = 0; j < current[i].contacts.length; j++) {
+        if(owner === current[i].contacts[j].name) {
+          gen = true;
+        }
+      }
+      //gen(erate) event if current user has permission to view it.
+      if(gen) {
+        //Get event start and end times
+        var startDate = new Date(current[i].start);
+        var endDate = new Date(current[i].end);
+
+        //Set bool to check if event should be displayed
+        var d1 = new Date(date);
+        var d2 = new Date(date);
+        d1.setHours(23); d1.setMinutes(59); d1.setSeconds(59);
+        d2.setHours(0); d2.setMinutes(0); d2.setMinutes(0);
+
+        //Display event if it starts or ends on day shown.
+        if(d1 >= startDate && d2 <= endDate) {
+          //GENERATE EVENTS HERE
+          stringOfEvents += '<li>'+current[i].name+'</li>';
+        }
+      }
+    }
+  }
+  return stringOfEvents;
+}
 
 function changeMonthPrev(){
   var date = new Date(localStorage.getItem('month_date'));
@@ -89,7 +139,7 @@ function changeMonthNext(){
 }
 
 function changeDay(event){
-  var dateNum = $(this).text();
+  var dateNum = parseInt($(this).text().substring(0,3));
   if(!isNaN(dateNum) && dateNum != 0) {
     var date = new Date(localStorage.getItem('month_date'));
     date.setDate(dateNum);
--- Redesign/js/days_events.js
+++ Redesign/js/days_events.js
@@ -83,20 +83,13 @@ function generateEvents() {
         var endHour = endDate.getHours();
         var endMin = endDate.getMinutes();
 
-        //Set bool to check if event should be displayed
-        var d = getDateShown_t();
-        var dayShown =  d.getMonth()+' '+d.getDate()+', '+d.getFullYear();
-        var startsToday = startDate.getMonth()+' '+startDate.getDate()+', '+startDate.getFullYear() == dayShown;
-        var endsToday = endDate.getMonth()+' '+endDate.getDate()+', '+endDate.getFullYear() == dayShown;
-        var checkYear = d.getFullYear() >= startDate.getFullYear() && d.getFullYear() <= endDate.getFullYear();
-        console.log(checkYear);
-        var checkMonth = d.getMonth() >= startDate.getMonth() && d.getMonth() <= endDate.getMonth();
-        console.log(checkMonth);
-        var checkDay = d.getDate() >= startDate.getDate() && d.getDate() <= endDate.getDate();
-        console.log(checkDay);
-
+        //d1 and d2 will be used to check if day should be displayed
+        var d1 = getDateShown_t();
+        var d2 = getDateShown_t();
+        d1.setHours(23); d1.setMinutes(59); d1.setSeconds(59);
+        d2.setHours(0); d2.setMinutes(0); d2.setMinutes(0);
         //Display event if it starts or ends on day shown.
-        if(checkYear && checkMonth && checkDay) {
+        if(d1 >= startDate && d2 <= endDate) {
           //Date formating
           if(startHour >= 13) {
             startTime = (startHour-12).toString()+':';
